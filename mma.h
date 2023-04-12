f599e62ba925da6b5dcf48482d9905f633f851cb
Robert A Alfieri <balfieri@nvidia.com>
2023-04-12 14:56:44 -0400

    checkpoint

--- mma.h
+++ mma.h
@@ -6,13 +6,7 @@ namespace nvcuda
 namespace wmma
 {
 
-class row_major
-{
-};
-
-class col_major
-{
-};
+#define mdassert(expr, msg) if ( !(expr) ) { std::cout << "ERROR: " << msg << "\n"; exit(1); }
 
 class accumulator
 {
@@ -26,6 +20,16 @@ class matrix_b
 {
 };
 
+// these are used for Layout below
+// note: col_major is standard for GEMM
+class row_major
+{
+};
+
+class col_major         
+{
+};
+
 using layout_t = uint32_t;
 const layout_t mem_row_major = 0;
 const layout_t mem_col_major = 1;
@@ -34,35 +38,67 @@ template<typename Use, int m, int n, int k, typename T, typename Layout=void>
 class fragment
 {
 public:
-    fragment(void)
+    fragment( void )
     {
-        num_elements = m*n; // not sure if this is right
+        mdassert( m == n && n == k, "fragment; m==n==k is currently required" );
+        num_elements = m*m; 
         x = new T[num_elements];
     }
 
     size_t num_elements;
-    T *    x;
+    T *    x;                   // note: for my sanity, always stored here in row-major regardless of Layout
 };
 
-template<typename Use, int m, int n, int k, typename T, typename Layout=void>
-void load_matrix_sync(fragment<Use, m, n, k, T, Layout> &a, const T* mptr, unsigned ldm)
+template<typename Use, int m, int n, int k, typename Layout=void>
+void fill_fragment( fragment<Use, m, n, k, float, Layout> &a, const float& v )
 {
-     (void)a;
-     (void)mptr;
-     (void)ldm;    
-     throw std::runtime_error{"load_matrix_sync not yet implemented"};
+    for( size_t i = 0; i < a.num_elements; i++ )
+    {
+        a.x[i] = v;
+    }
 }
 
-template<typename Use, int m, int n, int k, typename T, typename Layout=void>
-void load_matrix_sync(fragment<Use, m, n, k, T, Layout> &a, const T* mptr, unsigned ldm, layout_t layout)
+template<typename Use, int m, int n, int k, typename Layout=void>
+void fill_fragment( fragment<Use, m, n, k, __half, Layout> &a, const __half& v )
 {
-     (void)a;
-     (void)mptr;
-     (void)ldm;    
-     (void)layout;
-     throw std::runtime_error{"load_matrix_sync yet implemented"};
+    for( size_t i = 0; i < a.num_elements; i++ )
+    {
+        a.x[i] = v;
+    }
 }
 
+// mptr points to start of fragment in matrix.
+// ldm is col/row stride for col/row major fragment. 
+//
+template<typename Use, int m, int n, int k, typename T, typename Layout=void>
+void load_matrix_sync( fragment<Use, m, n, k, T, Layout> &a, const T* mptr, unsigned ldm )
+{
+    mdassert( ldm >= n, "load_matrix_sync: ldm (stride) too small" );
+    bool is_row_major = std::is_same<Layout, row_major>::value;
+    T * fptr = a.x;
+    if ( is_row_major ) {
+        for( uint32_t i = 0; i < m; i++ )
+        {
+            for( uint32_t j = 0; j < n; j++, fptr++, mptr++ )
+            {
+                *fptr = *mptr;
+            }
+
+            mptr += ldm - n;  // skip to beginning of next row
+        }
+    } else {
+        for( uint32_t i = 0; i < m; i++ )
+        {
+            for( uint32_t j = 0; j < n; j++, fptr++ )
+            {
+                *fptr = mptr[j*ldm + i];
+            }
+        }
+    }
+}
+
+// opposite of load_matrix_sync()
+//
 template<typename Use, int m, int n, int k, typename T, typename Layout=void>
 void store_matrix_sync(T* mptr, const fragment<Use, m, n, k, T, Layout> &a, unsigned ldm, layout_t layout)
 {
@@ -73,22 +109,8 @@ void store_matrix_sync(T* mptr, const fragment<Use, m, n, k, T, Layout> &a, unsi
      throw std::runtime_error{"store_matrix_sync not yet implemented"};
 }
 
-template<typename Use, int m, int n, int k, typename Layout=void>
-void fill_fragment(fragment<Use, m, n, k, float, Layout> &a, const float& v)
-{
-     (void)a;
-     (void)v;
-     throw std::runtime_error{"fill_fragment not yet implemented"};
-}
-
-template<typename Use, int m, int n, int k, typename Layout=void>
-void fill_fragment(fragment<Use, m, n, k, __half, Layout> &a, const __half& v)
-{
-     (void)a;
-     (void)v;
-     throw std::runtime_error{"fill_fragment not yet implemented"};
-}
-
+// perform d = a*b + c (typically d == c)
+//
 template<int m, int n, int k, typename Layout_d=void, typename Layout_a=void, typename Layout_b=void, typename Layout_c=void>
 void mma_sync(      fragment<accumulator, m, n, k, float, Layout_d> &d, 
               const fragment<matrix_a,    m, n, k, float, Layout_a> &a,
@@ -96,12 +118,12 @@ void mma_sync(      fragment<accumulator, m, n, k, float, Layout_d> &d,
               const fragment<accumulator, m, n, k, float, Layout_c> &c,
               bool  satf=false)
 {
-     (void)d;
-     (void)a;
-     (void)b;
-     (void)c;
-     (void)satf;
-     throw std::runtime_error{"mma_sync not yet implemented"};
+    (void)d;
+    (void)a;
+    (void)b;
+    (void)c;
+    (void)satf;
+    throw std::runtime_error{"mma_sync not yet implemented"};
 }
 
 template<int m, int n, int k, typename Layout_d=void, typename Layout_a=void, typename Layout_b=void, typename Layout_c=void>
@@ -111,12 +133,12 @@ void mma_sync(      fragment<accumulator, m, n, k, __half, Layout_d> &d,
               const fragment<accumulator, m, n, k, __half, Layout_c> &c,
               bool  satf=false)
 {
-     (void)d;
-     (void)a;
-     (void)b;
-     (void)c;
-     (void)satf;
-     throw std::runtime_error{"mma_sync not yet implemented"};
+    (void)d;
+    (void)a;
+    (void)b;
+    (void)c;
+    (void)satf;
+    throw std::runtime_error{"mma_sync not yet implemented"};
 }
 
 }; // wmma
